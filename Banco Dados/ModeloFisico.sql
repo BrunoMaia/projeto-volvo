33f3ad79ba95783c4c10b714c2f2974f8026babf
Bruno Maia <brunomaia@alunos.utfpr.edu.br>
2022-02-09 21:40:12 -0300

    Limpeza de arquivos

--- Banco Dados/ModeloFisico.sql
+++ Banco Dados/ModeloFisico.sql
@@ -1,52 +1,58 @@
 CREATE DATABASE Rede_Concessionaria;
-go
-USE Rede_Concessionaria;
-go
 
-CREATE TABLE Proprietario (
-    IdProprietario INT IDENTITY(1,1) PRIMARY KEY,
-    NomeProprietario VARCHAR(45) NOT NULL,
-    CadastroProprietario VARCHAR(14) NOT NULL,
-    EnderecoProprietarioCidade VARCHAR(45),
-    EnderecoProprietarioBairro VARCHAR(45),
-    EnderecoProprietarioLogradouro VARCHAR(45),
-    EnderecoProprietarioComplemento VARCHAR(45),
-    EnderecoProprietarioNumero INT,
-    EmailProprietario VARCHAR(45),
-    TelefoneProprietario VARCHAR(20),
-    UNIQUE (CadastroProprietario, EmailProprietario, TelefoneProprietario)
+USE Rede_Concessionaria;
+
+DROP TABLE Cliente;
+
+CREATE TABLE Cliente (
+    IdCliente INTEGER IDENTITY (1,1),
+    NomeCliente VARCHAR(45) NOT NULL,
+    CadastroCliente VARCHAR(14) UNIQUE NOT NULL,
+    EmailCliente VARCHAR(45) UNIQUE NOT NULL,
+    TelefoneCliente VARCHAR(45) UNIQUE NOT NULL,
+    EnderecoClienteCidade VARCHAR(45) NOT NULL,
+    EnderecoClienteBairro VARCHAR(45) NOT NULL,
+    EnderecoClienteLogradouro VARCHAR(45) NOT NULL,
+    EnderecoClienteNumero INTEGER NOT NULL,
+    EnderecoClienteComplemento VARCHAR(45),
+	PRIMARY KEY (IdCliente)
 );
 
 CREATE TABLE Veiculo (
-    IdVeiculo INT IDENTITY(1,1) PRIMARY KEY,
-    ChassiVeiculo VARCHAR(17) NOT NULL,
-    ModeloVeiculo VARCHAR(45),
-    AnoVeiculo INT,
-    CorVeiculo VARCHAR(45),
-    ValorVeiculo DECIMAL(20,2),
-    KmVeiculo INT NOT NULL,
-    AcessoriosVeiculo VARCHAR(90),
-    VersaoSistVeiculo DECIMAL(5,3) NOT NULL,
-    UNIQUE (ChassiVeiculo)
+    IdVeiculo INTEGER IDENTITY (1,1),
+    ChassiVeiculo VARCHAR(45) UNIQUE NOT NULL,
+    ModeloVeiculo VARCHAR(45) NOT NULL,
+    AnoVeiculo INTEGER NOT NULL,
+    CorVeiculo VARCHAR(45) NOT NULL,
+    ValorVeiculo DECIMAL (8,2) NOT NULL,
+    KmVeiculo INTEGER NOT NULL,
+    AcessoriosVeiculo VARCHAR(200),
+    VersaoSistVeiculo DECIMAL (5,5) NOT NULL,
+	PRIMARY KEY (IdVeiculo)
 );
 
 CREATE TABLE Vendedor (
-    MatriculaVendedor INT IDENTITY(1,1) PRIMARY KEY,
-    NomeVendedor VARCHAR(45),
-    CpfVendedor VARCHAR(13) NOT NULL ,
-    EmailVendedor VARCHAR(45)NOT NULL,
-    TelefoneVendedor VARCHAR(20) NOT NULL,
-    AdmissaoVendedor DATE,
-    VendasMesVendedor INT,
-    VendasTotalVendedor INT,
-    SalarioVendedor DECIMAL(20,2),
-    UNIQUE (MatriculaVendedor, CpfVendedor, EmailVendedor, TelefoneVendedor)
+    MatriculaVendedor INTEGER IDENTITY (1,1),
+    NomeVendedor VARCHAR(45) NOT NULL,
+    CpfVendedor VARCHAR(11) UNIQUE NOT NULL,
+    EmailVendedor VARCHAR(45) UNIQUE NOT NULL,
+    TelefoneVendedor VARCHAR(45) UNIQUE NOT NULL,
+    AdmissaoVendedor DATE NOT NULL,
+    VendasMesVendedor INTEGER NOT NULL,
+    VendasTotalVendedor INTEGER NOT NULL,
+    SalarioVendedor DECIMAL (8,2) NOT NULL,
+	PRIMARY KEY (MatriculaVendedor)
 );
 
-CREATE TABLE Vendas (
-    IdVendas INT IDENTITY(1,1) PRIMARY KEY,
-    MatriculaVendedor INT,
-    IdVeiculo INT FOREIGN KEY REFERENCES Veiculo(IdVeiculo),
-    DataVendas DATE,
-    IdProprietario INT FOREIGN KEY REFERENCES Proprietario(IdProprietario)
+CREATE TABLE Venda (
+    IdVendas INTEGER IDENTITY (1,1),
+    MatriculaVendedor INTEGER NOT NULL,
+    IdVeiculo INTEGER NOT NULL,
+    IdCliente INTEGER NOT NULL,
+    DataVenda DATE NOT NULL,
+	PRIMARY KEY (IdVendas),
+	FOREIGN KEY (MatriculaVendedor) REFERENCES Vendedor(MatriculaVendedor),
+	FOREIGN KEY (IdVeiculo) REFERENCES Veiculo(idVeiculo),
+	FOREIGN KEY (IdCliente) REFERENCES Cliente(idCliente)
 );
+ 
